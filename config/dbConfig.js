4f537170699da1d1e53b791529339eb74bc506ce
Fatimah Yasin <113253050+Fatimahbintiyasin@users.noreply.github.com>
2024-05-17 22:46:54 -0400

    Add files via upload

---
+++ config/dbConfig.js
@@ -0,0 +1,14 @@
+// dbConfig.js
+import oracledb from 'oracledb';
+
+const dbConfig = {
+    user: 'COMP214_m23_er_70',
+    password: 'Tariks',
+    connectString:'199.212.26.208:1521/SQLD',
+  };
+
+
+  export const SECRET = 'your_secret_key'; 
+
+
+export default dbConfig;
